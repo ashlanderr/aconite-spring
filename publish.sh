44bc83b1788b6d624849ffc713a8b2f889abcd5e
Александр Шилов <aleksandr.schilow2012@gmail.com>
2019-05-07 12:27:31 +0300

    Add publication to private sonatype nexus repository

---
+++ publish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+./gradlew aconite-data-spring-boot-starter:uploadArchives \
+          aconite-retrofit-spring-boot-starter:uploadArchives \
+          aconite-webflux-error-spring-boot-starter:uploadArchives \
+          aconite-webflux-server-spring-boot-starter:uploadArchives
